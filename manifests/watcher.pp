a952243b838b29a0730188882c7194358dfc7cfa
jv <jeff@jeffvier.com>
2013-07-12 18:49:12 -0700

    add usage example

--- manifests/watcher.pp
+++ manifests/watcher.pp
@@ -1,3 +1,11 @@
+## Usage:
+##  incron::watcher {
+##    'soap_inputDir':
+##      location => '/home/mule/inputDir',
+##      mask     => 'IN_CREATE',
+##      action   => 'runuser - filemover -c \'scp -p $@ destination.example.com:/data/dump/\'';
+##   }
+
 define incron::watcher($ensure = present, $location, $mask, $action) {
 
   include incron
